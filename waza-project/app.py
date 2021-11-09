e6665e22823c6b995354b990b0fbd4355962e531
gardy291 <edgardo.figueroa5@upr.edu>
2021-11-08 21:11:52 -0500

    added class room to models and route to app.py

--- waza-project/app.py
+++ waza-project/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify, abort
 import config
 from model.meeting import Meeting
+from model.room import Room
 
 app = Flask(__name__)
 
-
+#------------------------------------
+# app routes for Meeting
+#------------------------------------
 @app.route('/waza/meeting/', methods=['POST', 'GET'])
 def meetings():
     connection = config.connection
@@ -49,6 +52,53 @@ def meetings_detail(meeting_id):
     else:
         return meet
 
+#------------------------------------
+# app routes for Room
+#------------------------------------
+
+@app.route('/waza/room/', methods=['POST', 'GET'])
+def room():
+    connection = config.connection
+    if request.method == "POST":
+        roomtype_id = request.form.get('roomtype_id', '')
+        department_id = request.form.get('department_id', '')
+        name = request.form.get('name', '')
+        capacity = request.form.get('capacity', '')
+        room_id = Room.post(connection, roomtype_id, department_id, name, capacity)
+        return {
+            "message": ("Room with id %s was inserted" % (room_id))
+        }, 200
+    else:
+        response = jsonify(Room.get_all(connection))
+        response.status_code = 200
+        return response
+
+
+@app.route('/waza/room/<int:room_id>', methods=['DELETE', 'GET', 'PUT'])
+def room_detail(room_id):
+    connection = config.connection
+    room = Room.get_first(connection, room_id)
+    if not room:
+        abort(404)
+
+    if request.method == 'DELETE':
+        Room.delete(connection, room["id"])
+        return {
+            "message": ("Room with id %s was removed" % (room["id"]))
+        }, 200
+
+    elif request.method == 'PUT':
+        roomtype_id = request.form.get('roomtype_id', '')
+        department_id = request.form.get('department_id', '')
+        name = request.form.get('name', '')
+        capacity = request.form.get('capacity', '')
+        Meeting.put(connection, room["id"], roomtype_id, department_id, name, capacity)
+        return {
+            "message": ("Room with id %s was updated" % (room["id"]))
+        }, 200
+
+    else:
+        return room
 
 if __name__ == '__main__':
     app.run()
